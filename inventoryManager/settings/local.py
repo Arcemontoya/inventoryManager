ad1bc759c0e74fcf59a86bde2ce73e7291cdc4ba
Pepe Arce <a1271264@uabc.edu.mx>
2025-06-28 13:14:21 -0700

    Local server config

--- inventoryManager/settings/local.py
+++ inventoryManager/settings/local.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3!1=x)*$=h@f7#124xjjul2zjj*3c7a$c2%(1e58z_7dw7i=5-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','*']
 
 
 # Application definition
